e9b8f1a8fbf4722d4f8e9c96e53fdda0f276d407
Ahamed Fahmi <dev.ahamedfahmi@gmail.com>
2021-12-01 16:02:09 +0530

    Started a new project

---
+++ Small & Big Number/README.md
@@ -0,0 +1,5 @@
+This is an simple program 
+
+This take 2 integer input from users 
+
+Then it will return which is big one and small one
---
+++ Small & Big Number/src/App.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        //Declare Variables
+        int num1;
+        int num2;
+
+        //get input from user
+        Scanner input1 = new Scanner(System.in);
+        System.out.println("Enter Number 1 := ");
+        input1.nextInt();
+    
+
+        Scanner input2 = new Scanner (System.in);
+        System.out.println("Enter Number 2");
+        input2.nextInt();
+
+        //conditions
+        
+        
+
+
+
+
+    }
+}
